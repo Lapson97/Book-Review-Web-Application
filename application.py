c8ea643ea393dc794fc329976ea8ed25a628a85d
Lapson97 <51078264+Lapson97@users.noreply.github.com>
2020-05-14 11:13:02 +0200

    Added API page and css

--- application.py
+++ application.py
@@ -7,7 +7,8 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from credentials import key, secret
 import requests
 
-app = Flask(__name__, static_folder=os.path.join(os.getcwd(), 'static'), static_url_path='')
+
+app = Flask(__name__)
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
@@ -16,6 +17,9 @@ if not os.getenv("DATABASE_URL"):
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+app.config['ENV'] = 'development'
+app.config['DEBUG'] = True
+app.config['TESTING'] = True
 Session(app)
 
 # Set up database
@@ -61,6 +65,7 @@ def register():
 # Comes after logging in
 @app.route("/search", methods=["GET", "POST"])
 def search():
+    #email = session["user_email"]
     # Request from /login to log a user in
     if request.method == "POST":
         # Checking if the user is present, if not present show error message and redirect to /register
@@ -75,6 +80,8 @@ def search():
         session["user_id"] = user.id
     if request.method == "GET" and "user_email" not in session:
         return render_template("login.html", error_message="Please login first", work="Login")
+    else:
+        return render_template("search.html", user_email=session["user_email"])
     return render_template("search.html", user_email=email)
 
 
@@ -159,5 +166,34 @@ def detail(book_id):
     return render_template("detail.html", book=book, users=users, ratings_count=ratings_count, average_rating=average_rating, user_email=session["user_email"])
 
 
+# Page for API
+@app.route("/api/<ISBN>")
+def api(ISBN):
+    book = db.execute("SELECT * FROM books WHERE isbn=:ISBN", {"ISBN": ISBN}).fetchone()
+    if book is None:
+        return render_template("error.html", error_message="Invalid ISBN. Please check for errors and try again.")
+    
+    reviews = db.execute("SELECT * FROM reviews WHERE book_id=:book_id", {"book_id": book.id}).fetchall()
+    count = 0
+    rating = 0
+    for review in reviews:
+        count += 1
+        rating += review.rating
+    if count:
+        average_rating = rating / count
+    else:
+        average_rating = 0
+
+    return jsonify(
+        title=book.title,
+        author=book.author,
+        year=book.year,
+        isbn=book.isbn,
+        review_count=count,
+        average_score=average_rating
+    )
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.debug = True
+    app.run()
